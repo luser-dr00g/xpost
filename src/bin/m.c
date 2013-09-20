1bd792bac39c1ca8849a1aaff90fc2d36ee6587a
M Joshua Ryan <luser.droog@gmail.com>
2013-09-20 15:50:06 -0500

    changed C++-style casts in borrowed _WIN32 mode_t code.

--- src/bin/m.c
+++ src/bin/m.c
@@ -26,15 +26,15 @@ typedef int mode_t;
 static const mode_t S_ISUID      = 0x08000000;           ///< does nothing
 static const mode_t S_ISGID      = 0x04000000;           ///< does nothing
 static const mode_t S_ISVTX      = 0x02000000;           ///< does nothing
-static const mode_t S_IRUSR      = mode_t(_S_IREAD);     ///< read by user
-static const mode_t S_IWUSR      = mode_t(_S_IWRITE);    ///< write by user
+static const mode_t S_IRUSR      = (mode_t)_S_IREAD;     ///< read by user
+static const mode_t S_IWUSR      = (mode_t)_S_IWRITE;    ///< write by user
 static const mode_t S_IXUSR      = 0x00400000;           ///< does nothing
 #   ifndef STRICT_UGO_PERMISSIONS
-static const mode_t S_IRGRP      = mode_t(_S_IREAD);     ///< read by *USER*
-static const mode_t S_IWGRP      = mode_t(_S_IWRITE);    ///< write by *USER*
+static const mode_t S_IRGRP      = (mode_t)_S_IREAD;     ///< read by *USER*
+static const mode_t S_IWGRP      = (mode_t)_S_IWRITE;    ///< write by *USER*
 static const mode_t S_IXGRP      = 0x00080000;           ///< does nothing
-static const mode_t S_IROTH      = mode_t(_S_IREAD);     ///< read by *USER*
-static const mode_t S_IWOTH      = mode_t(_S_IWRITE);    ///< write by *USER*
+static const mode_t S_IROTH      = (mode_t)_S_IREAD;     ///< read by *USER*
+static const mode_t S_IWOTH      = (mode_t)_S_IWRITE;    ///< write by *USER*
 static const mode_t S_IXOTH      = 0x00010000;           ///< does nothing
 #   else
 static const mode_t S_IRGRP      = 0x00200000;           ///< does nothing
