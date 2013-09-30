e08764d5f0dfee6e80d7923e1fefebbd7462a0d4
Vincent Torri <vincent.torri@gmail.com>
2013-09-30 11:45:44 +0200

    fix warnings

--- src/bin/f.c
+++ src/bin/f.c
@@ -2,6 +2,32 @@
 # include <config.h>
 #endif
 
+#ifdef STDC_HEADERS
+# include <stdlib.h>
+# include <stddef.h>
+#else
+# ifdef HAVE_STDLIB_H
+#  include <stdlib.h>
+# endif
+#endif
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif !defined alloca
+# ifdef __GNUC__
+#  define alloca __builtin_alloca
+# elif defined _AIX
+#  define alloca __alloca
+# elif defined _MSC_VER
+#  include <malloc.h>
+#  define alloca _alloca
+# elif !defined HAVE_ALLOCA
+#  ifdef  __cplusplus
+extern "C"
+#  endif
+void *alloca (size_t);
+# endif
+#endif
+
 #ifdef HAVE_STDBOOL_H
 # include <stdbool.h>
 #else
@@ -21,6 +47,7 @@ typedef bool _Bool;
 #include <errno.h>
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -41,7 +68,7 @@ typedef bool _Bool;
  * privileges.
  */
 static FILE *
-f_tmpfile()
+f_tmpfile(void)
 {
   char *buf;
   const char *name;
