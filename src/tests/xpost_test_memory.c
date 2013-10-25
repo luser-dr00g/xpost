5e3277189e41003ecd96cfc0c95e75484653b0bc
M Joshua Ryan <luser.droog@gmail.com>
2013-10-25 01:54:22 -0500

    add first unit test.

--- src/tests/xpost_test_memory.c
+++ src/tests/xpost_test_memory.c
@@ -5,11 +5,16 @@
 #include <stdio.h>
 
 #include <check.h>
+#include "../lib/xpost_memory.h"
 
 #include "xpost_suite.h"
 
 START_TEST(xpost_init)
 {
+    Xpost_Memory_File mem;
+    ck_assert_int_eq (xpost_memory_file_init(&mem, "", -1), 1);
+    ck_assert_int_eq (xpost_memory_file_grow(&mem, 4096), 1);
+    ck_assert_int_eq (xpost_memory_file_exit(&mem), 1);
 }
 END_TEST
 
