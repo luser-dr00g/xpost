b95f76fa7f74c85c0a96c23214b42afa46939424
Vincent Torri <vincent.torri@gmail.com>
2013-11-09 10:31:44 +0100

    fix CreateFileMapping when size if 65536 bytes minor log improvements

---
+++ src/bin/xpost_main.h
@@ -0,0 +1,62 @@
+/*
+ * Xpost - a Level-2 Postscript interpreter
+ * Copyright (C) 2013, Michael Joshua Ryan
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the Xpost software product nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+
+#include <assert.h>
+#include <setjmp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#include "xpost_log.h"
+#include "xpost_memory.h"  // itp contexts contain mfiles and mtabs
+#include "xpost_object.h"  // eval functions examine objects
+#include "xpost_stack.h"  // eval functions manipulate stacks
+#include "xpost_context.h"
+#include "xpost_interpreter.h" // uses: context itp MAXCONTEXT MAXMFILE
+#include "xpost_error.h"  // interpreter catches errors
+#include "xpost_string.h"  // eval functions examine strings
+#include "xpost_array.h"  // eval functions examine arrays
+#include "xpost_garbage.h"  // interpreter initializes garbage collector
+#include "xpost_save.h"  // interpreter initializes save/restore stacks
+#include "xpost_name.h"  // eval functions examine names
+#include "xpost_dict.h"  // eval functions examine dicts
+#include "xpost_file.h"  // eval functions examine files
+#include "xpost_operator.h"  // eval functions call operators
+#include "xpost_op_token.h"  // token operator functions
+#include "xpost_op_dict.h"  // dictionary operator functions
+#include "xpost_pathname.h"
+
+
