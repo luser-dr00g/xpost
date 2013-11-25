954387fd087d35174eda82543a9b6ea7097835b1
M Joshua Ryan <luser.droog@gmail.com>
2013-11-25 07:35:22 -0600

    make free alloc wastage parameter a manifest constant. reduce erasepage output, increase FillPoly output.

--- src/lib/xpost_free.c
+++ src/lib/xpost_free.c
@@ -232,7 +232,8 @@ int xpost_free_alloc(Xpost_Memory_File *mem,
 
         /* if this ent is sufficient to hold sz,
            but does not waste more than sz bytes, use it */
-        if (tsz >= sz && tsz <= sz * 2)
+        if (tsz >= sz &&
+                tsz <= sz * XPOST_FREE_ACCEPT_OVERSIZE)
         {
             Xpost_Memory_Table *tab;
             unsigned int ent;
