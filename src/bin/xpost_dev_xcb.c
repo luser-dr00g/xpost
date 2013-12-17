9f8d7291d2a21c015600af7f6ba07bb0fcabd536
Vincent Torri <vincent.torri@gmail.com>
2013-12-17 07:27:40 +0100

    add title to the window add alloca() declaration in xcb device

--- src/bin/xpost_dev_xcb.c
+++ src/bin/xpost_dev_xcb.c
@@ -32,6 +32,32 @@
 # include <config.h>
 #endif
 
+#ifdef STDC_HEADERS
+# include <stdlib.h>
+# include <stddef.h>
+#else
+# ifdef HAVE_STDLIB_H
+#  include <stdlib.h>
+# endif
+#endif
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif !defined alloca
+# ifdef __GNUC__
+#  define alloca __builtin_alloca
+# elif defined _AIX
+#  define alloca __alloca
+# elif defined _MSC_VER
+#  include <malloc.h>
+#  define alloca _alloca
+# elif !defined HAVE_ALLOCA
+#  ifdef  __cplusplus
+extern "C"
+#  endif
+void *alloca (size_t);
+# endif
+#endif
+
 #include <assert.h>
 #include <stdlib.h> /* abs */
 #include <string.h>
@@ -39,6 +65,7 @@
 #include <xcb/xcb.h>
 #include <xcb/xcb_image.h>
 #include <xcb/xcb_aux.h>
+#include <xcb/xcb_icccm.h>
 
 #include "xpost_log.h"
 #include "xpost_memory.h" /* access memory */
@@ -220,6 +247,7 @@ int _create_cont (Xpost_Context *ctx,
                 private.scr->root_visual,
                 XCB_CW_BACK_PIXEL,
                 &value);
+        xcb_icccm_set_wm_name(private.c, private.win, XCB_ATOM_STRING, 8, strlen("Xpost"), "Xpost");
     }
     xcb_map_window(private.c, private.win);
     xcb_flush(private.c);
