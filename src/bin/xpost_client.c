d4528a32bff199977d51942621e2fb83bcbafc71
M Joshua Ryan <luser.droog@gmail.com>
2014-07-28 12:09:24 -0500

    add example program for library usage.

---
+++ src/bin/xpost_client.c
@@ -0,0 +1,25 @@
+/*
+   This is a simple example of a client calling xpost as a library
+   with a postscript program, desiring the raster data of the 
+   generated image.
+
+TODO:
+    add to makefile and make compilable
+    pass ps program to xpost
+    get raster data
+ */
+
+#include "xpost.h"
+#include "xpost_interpreter.h"
+
+char *prog =
+    "300 400 100 0 360 arc\n"
+    "fill\n"
+    "showpage\n";
+
+int main() {
+    xpost_create("ppm", NULL, "/usr/local/bin", 1);
+    xpost_run();
+    xpost_destroy();
+}
+
