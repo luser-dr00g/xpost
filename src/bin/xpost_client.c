4a8c6d2e0c3bccc9f6c1cdea6ca70f39879135e5
M Joshua Ryan <luser.droog@gmail.com>
2014-09-03 04:07:15 -0500

    add bounding box to ps example in xpost_client. To render an eps as an image, client should parse BoundingBox string for bounds and pass the geometry to xpost.

--- src/bin/xpost_client.c
+++ src/bin/xpost_client.c
@@ -13,13 +13,18 @@ TODO:
 #include "xpost_interpreter.h"
 
 char *prog =
+    "%%BoundingBox: 200 300 400 500\n"
     "300 400 100 0 360 arc\n"
     "fill\n"
     "showpage\n";
 
 int main() {
     void *buffer_type_object;
-    xpost_create("bgr", XPOST_OUTPUT_BUFFEROUT, &buffer_type_object, XPOST_SHOWPAGE_RETURN, 1);
+    xpost_create("bgr",
+            XPOST_OUTPUT_BUFFEROUT,
+            &buffer_type_object,
+            XPOST_SHOWPAGE_RETURN,
+            1);
     xpost_run(XPOST_INPUT_STRING, prog);
     xpost_destroy();
     free(buffer_type_object);
