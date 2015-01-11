f83411dfb8a25cf3f9629f6674c910a93b033d72
Vincent Torri <vincent dot torri at gmail dot com@localhost>
2015-01-11 09:17:30 +0100

    forgor to add config.h

---
+++ visual_studio/config.h
@@ -0,0 +1,15 @@
+
+/* Define to the relative data directory of the library */
+#define PACKAGE_DATA_DIR "/share/xpost"
+
+/* Define to the relative install directory of the library */
+#define PACKAGE_INSTALL_DIR "."
+
+/* Define to the major version of the library */
+#define XPOST_VERSION_MAJ 0
+
+/* Define to the micro version of the library */
+#define XPOST_VERSION_MIC 1
+
+/* Define to the minor version of the library */
+#define XPOST_VERSION_MIN 0
