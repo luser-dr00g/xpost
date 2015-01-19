de15b6a038bf2bf00a06db2a725913d66201d945
M Joshua Ryan <luser.droog@gmail.com>
2015-01-18 18:18:37 -0600

    update/add doxygen comments.

--- src/lib/xpost_string.h
+++ src/lib/xpost_string.h
@@ -31,39 +31,80 @@
 #ifndef XPOST_STRING_H
 #define XPOST_STRING_H
 
-/* strings
-   */
+/**
+ * @file xpost_string.h
+ * @brief string functions
+ *
+ * An string object is 8 bytes,
+ * consisting of 4 16bit fields common to all composite objects
+ *   tag, type enum and flags
+ *   sz, count of objects in array
+ *   ent, entity number   --- nb. ents have outgrown their field: use xpost_object_get/set_ent()
+ *   off, offset into allocation
+ * the entity data is a "C" array of chars
+ *
+ * "_memory" functions require a memory file to be specified.
+ * functions without "memory" select the memory file from a context, using the FBANK flag.
+ * an array object with the FBANK flag properly set, is called a "banked array".
+ *
+ * @{
+ */
 
+/**
+ * @brief construct a string object (possibly initialized) in the specified memory
+ */
 Xpost_Object xpost_string_cons_memory(Xpost_Memory_File *mem,
                      unsigned sz,
                      /*@NULL@*/ const char *ini);
 
+/**
+ * @brief construct a string object in currectly selected memory
+ */
 Xpost_Object xpost_string_cons(Xpost_Context *ctx,
                      unsigned sz,
                      /*@NULL@*/ const char *ini);
 
+/**
+ * @brief yield a "C" pointer to the char array of the string contents
+ */
 /*@dependent@*/
 char *xpost_string_get_pointer(Xpost_Context *ctx,
               Xpost_Object S);
 
+/**
+ * @brief put a value into a string with specified memory
+ */
 int xpost_string_put_memory(Xpost_Memory_File *mem,
            Xpost_Object s,
            integer i,
            integer c);
 
+/**
+ * @brief put a value into a string
+ */
 int xpost_string_put(Xpost_Context *ctx,
            Xpost_Object s,
            integer i,
            integer c);
 
+/**
+ * @brief get a value from a string with specified memory
+ */
 int xpost_string_get_memory(Xpost_Memory_File *mem,
            Xpost_Object s,
            integer i,
            integer *retval);
 
+/**
+ * @brief get a value from a string
+ */
 int xpost_string_get(Xpost_Context *ctx,
            Xpost_Object s,
            integer i,
            integer *retval);
+
+/**
+ * @}
+ */
 
 #endif
