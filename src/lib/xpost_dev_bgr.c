dcc9e0329105962ccb0da9ea0bc998b5cfa0990d
Vincent Torri <vincent.torri@gmail.com>
2016-06-01 07:32:11 +0200

    bgr device: formatting

--- src/lib/xpost_dev_bgr.c
+++ src/lib/xpost_dev_bgr.c
@@ -82,10 +82,10 @@ static unsigned int _create_cont_opcode;
 /* create an instance of the device
    using the class .copydict procedure */
 static
-int _create (Xpost_Context *ctx,
-             Xpost_Object width,
-             Xpost_Object height,
-             Xpost_Object classdic)
+int _create(Xpost_Context *ctx,
+            Xpost_Object width,
+            Xpost_Object height,
+            Xpost_Object classdic)
 {
     xpost_stack_push(ctx->lo, ctx->os, width);
     xpost_stack_push(ctx->lo, ctx->os, height);
@@ -98,10 +98,12 @@ int _create (Xpost_Context *ctx,
     /* call device class's ps-level .copydict procedure,
        //call base-class's Create procedure (to initialize ImgData array)
        then call _create_cont, by continuation. */
-    if (!xpost_stack_push(ctx->lo, ctx->es, xpost_operator_cons_opcode(_create_cont_opcode)))
+    if (!xpost_stack_push(ctx->lo, ctx->es,
+                          xpost_operator_cons_opcode(_create_cont_opcode)))
         return execstackoverflow;
 
-    if (!xpost_stack_push(ctx->lo, ctx->es, xpost_dict_get(ctx, classdic, namedotcopydict)))
+    if (!xpost_stack_push(ctx->lo, ctx->es,
+                          xpost_dict_get(ctx, classdic, namedotcopydict)))
         return execstackoverflow;
 
     return 0;
@@ -110,10 +112,10 @@ int _create (Xpost_Context *ctx,
 /* initialize the C-level data
    and define in the device instance */
 static
-int _create_cont (Xpost_Context *ctx,
-                  Xpost_Object w,
-                  Xpost_Object h,
-                  Xpost_Object devdic)
+int _create_cont(Xpost_Context *ctx,
+                 Xpost_Object w,
+                 Xpost_Object h,
+                 Xpost_Object devdic)
 {
     Xpost_Object privatestr;
     PrivateData private;
@@ -171,10 +173,10 @@ int _create_cont (Xpost_Context *ctx,
             row = xpost_object_cvlit(xpost_array_cons(ctx, width));
             xpost_array_put(ctx, imgdata, i, row);
             xpost_memory_put(xpost_context_select_memory(ctx, row),
-                    xpost_object_get_ent(row),
-                    0,
-                    width * sizeof(Xpost_Object),
-                    rowdata);
+                             xpost_object_get_ent(row),
+                             0,
+                             width * sizeof(Xpost_Object),
+                             rowdata);
         }
         xpost_dict_put(ctx, devdic, xpost_name_cons(ctx, "ImgData"), imgdata);
 
@@ -184,7 +186,8 @@ int _create_cont (Xpost_Context *ctx,
 
     /* save private data struct in string */
     xpost_memory_put(xpost_context_select_memory(ctx, privatestr),
-            xpost_object_get_ent(privatestr), 0, sizeof private, &private);
+                     xpost_object_get_ent(privatestr), 0,
+                     sizeof(private), &private);
 
     /* return device instance dictionary to ps */
     xpost_stack_push(ctx->lo, ctx->os, devdic);
@@ -194,13 +197,13 @@ int _create_cont (Xpost_Context *ctx,
 #ifdef FAST_C_BUFFER
 
 static
-int _putpix (Xpost_Context *ctx,
-             Xpost_Object red,
-             Xpost_Object green,
-             Xpost_Object blue,
-             Xpost_Object x,
-             Xpost_Object y,
-             Xpost_Object devdic)
+int _putpix(Xpost_Context *ctx,
+            Xpost_Object red,
+            Xpost_Object green,
+            Xpost_Object blue,
+            Xpost_Object x,
+            Xpost_Object y,
+            Xpost_Object devdic)
 {
     Xpost_Object privatestr;
     PrivateData private;
@@ -228,7 +231,8 @@ int _putpix (Xpost_Context *ctx,
     if (xpost_object_get_type(privatestr) == invalidtype)
         return undefined;
     xpost_memory_get(xpost_context_select_memory(ctx, privatestr),
-            xpost_object_get_ent(privatestr), 0, sizeof private, &private);
+                     xpost_object_get_ent(privatestr), 0,
+                     sizeof(private), &private);
 
     /* check bounds */
     if (x.int_.val < 0 || x.int_.val >= xpost_dict_get(ctx, devdic, namewidth).int_.val)
@@ -246,7 +250,8 @@ int _putpix (Xpost_Context *ctx,
 
     /* save private data struct in string */
     xpost_memory_put(xpost_context_select_memory(ctx, privatestr),
-            xpost_object_get_ent(privatestr), 0, sizeof private, &private);
+                     xpost_object_get_ent(privatestr), 0,
+                     sizeof(private), &private);
 
     return 0;
 }
@@ -254,7 +259,7 @@ int _putpix (Xpost_Context *ctx,
 #endif
 
 static
-int _flush (Xpost_Context *ctx,
+int _flush(Xpost_Context *ctx,
            Xpost_Object devdic)
 {
     Xpost_Object privatestr;
@@ -265,15 +270,16 @@ int _flush (Xpost_Context *ctx,
     if (xpost_object_get_type(privatestr) == invalidtype)
         return undefined;
     xpost_memory_get(xpost_context_select_memory(ctx, privatestr),
-            xpost_object_get_ent(privatestr), 0, sizeof private, &private);
+                     xpost_object_get_ent(privatestr), 0,
+                     sizeof(private), &private);
 
     return 0;
 }
 
 
 static
-int _emit (Xpost_Context *ctx,
-           Xpost_Object devdic)
+int _emit(Xpost_Context *ctx,
+          Xpost_Object devdic)
 {
     Xpost_Object privatestr;
     PrivateData private;
@@ -289,7 +295,8 @@ int _emit (Xpost_Context *ctx,
     if (xpost_object_get_type(privatestr) == invalidtype)
         return undefined;
     xpost_memory_get(xpost_context_select_memory(ctx, privatestr),
-            xpost_object_get_ent(privatestr), 0, sizeof private, &private);
+                     xpost_object_get_ent(privatestr), 0,
+                     sizeof(private), &private);
 
 #ifdef FAST_C_BUFFER
     data = (unsigned char *)private.buf->data;
@@ -313,7 +320,7 @@ int _emit (Xpost_Context *ctx,
 
         mem = xpost_context_select_memory(ctx, imgdata);
 
-        for (i=0; i < height; i++)
+        for (i = 0; i < height; i++)
         {
             row = xpost_array_get_memory(mem, imgdata, i);
             //row = xpost_array_get(ctx, imgdata, i);
@@ -321,7 +328,7 @@ int _emit (Xpost_Context *ctx,
             rowdata = (Xpost_Object *)(mem->base + rowaddr);
             //printf("%d\n", i);
 
-            for (j=0; j < stride; j++)
+            for (j = 0; j < stride; j++)
             {
                 unsigned int val;
                 val = rowdata[j].int_.val; /* r|g|b 0x00RRGGGBB */
@@ -338,7 +345,8 @@ int _emit (Xpost_Context *ctx,
         Xpost_Object sd, outbufstr;
         sd = xpost_stack_bottomup_fetch(ctx->lo, ctx->ds, 0);
         outbufstr = xpost_dict_get(ctx, sd, xpost_name_cons(ctx, "OutputBufferOut"));
-        if (xpost_object_get_type(outbufstr) == stringtype){
+        if (xpost_object_get_type(outbufstr) == stringtype)
+        {
             unsigned char **outbuf;
             memcpy(&outbuf, xpost_string_get_pointer(ctx, outbufstr), sizeof(outbuf));
             *outbuf = data;
@@ -354,9 +362,9 @@ int _emit (Xpost_Context *ctx,
    installed in userdict by calling 'loadXXXdevice'.
  */
 static
-int newbgrdevice (Xpost_Context *ctx,
-                  Xpost_Object width,
-                  Xpost_Object height)
+int newbgrdevice(Xpost_Context *ctx,
+                 Xpost_Object width,
+                 Xpost_Object height)
 {
     Xpost_Object classdic;
     int ret;
@@ -367,7 +375,8 @@ int newbgrdevice (Xpost_Context *ctx,
     if (ret)
         return ret;
     classdic = xpost_stack_topdown_fetch(ctx->lo, ctx->os, 0);
-    if (!xpost_stack_push(ctx->lo, ctx->es, xpost_dict_get(ctx, classdic, xpost_name_cons(ctx, "Create"))))
+    if (!xpost_stack_push(ctx->lo, ctx->es,
+                          xpost_dict_get(ctx, classdic, xpost_name_cons(ctx, "Create"))))
         return execstackoverflow;
 
     return 0;
@@ -382,7 +391,7 @@ unsigned int _loadbgrdevicecont_opcode;
    call loadbgrdevicecont by continuation.
  */
 static
-int loadbgrdevice (Xpost_Context *ctx)
+int loadbgrdevice(Xpost_Context *ctx)
 {
     Xpost_Object classdic;
     int ret;
@@ -391,9 +400,11 @@ int loadbgrdevice (Xpost_Context *ctx)
     if (ret)
         return ret;
     classdic = xpost_stack_topdown_fetch(ctx->lo, ctx->os, 0);
-    if (!xpost_stack_push(ctx->lo, ctx->es, xpost_operator_cons_opcode(_loadbgrdevicecont_opcode)))
+    if (!xpost_stack_push(ctx->lo, ctx->es,
+                          xpost_operator_cons_opcode(_loadbgrdevicecont_opcode)))
         return execstackoverflow;
-    if (!xpost_stack_push(ctx->lo, ctx->es, xpost_dict_get(ctx, classdic, namedotcopydict)))
+    if (!xpost_stack_push(ctx->lo, ctx->es,
+                          xpost_dict_get(ctx, classdic, namedotcopydict)))
         return execstackoverflow;
 
     return 0;
@@ -404,8 +415,8 @@ int loadbgrdevice (Xpost_Context *ctx)
    defines a new operator in userdict: newbgrdevice
  */
 static
-int loadbgrdevicecont (Xpost_Context *ctx,
-                       Xpost_Object classdic)
+int loadbgrdevicecont(Xpost_Context *ctx,
+                      Xpost_Object classdic)
 {
     Xpost_Object userdict;
     Xpost_Object op;
@@ -422,9 +433,9 @@ int loadbgrdevicecont (Xpost_Context *ctx,
 
 #ifdef FAST_C_BUFFER
     op = xpost_operator_cons(ctx, "bgrPutPix", (Xpost_Op_Func)_putpix, 0, 6,
-            numbertype, numbertype, numbertype,
-            numbertype, numbertype,
-            dicttype);
+                             numbertype, numbertype, numbertype,
+                             numbertype, numbertype,
+                             dicttype);
     ret = xpost_dict_put(ctx, classdic, xpost_name_cons(ctx, "PutPix"), op);
     if (ret)
         return ret;
@@ -458,8 +469,8 @@ int loadbgrdevicecont (Xpost_Context *ctx,
    install the loadXXXdevice which may be called during graphics initialization
    to produce the operator newXXXdevice which instantiates the device dictionary.
 */
-int xpost_oper_init_bgr_device_ops (Xpost_Context *ctx,
-                Xpost_Object sd)
+int xpost_oper_init_bgr_device_ops(Xpost_Context *ctx,
+                                   Xpost_Object sd)
 {
     unsigned int optadr;
     Xpost_Operator *optab;
@@ -480,7 +491,8 @@ int xpost_oper_init_bgr_device_ops (Xpost_Context *ctx,
         return VMerror;
 
     xpost_memory_table_get_addr(ctx->gl,
-            XPOST_MEMORY_TABLE_SPECIAL_OPERATOR_TABLE, &optadr);
+                                XPOST_MEMORY_TABLE_SPECIAL_OPERATOR_TABLE,
+                                &optadr);
     optab = (Xpost_Operator *)(ctx->gl->base + optadr);
     op = xpost_operator_cons(ctx, "loadbgrdevice", (Xpost_Op_Func)loadbgrdevice, 1, 0); INSTALL;
     op = xpost_operator_cons(ctx, "loadbgrdevicecont", (Xpost_Op_Func)loadbgrdevicecont, 1, 1, dicttype);
