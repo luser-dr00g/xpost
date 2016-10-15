880d531e574150744f4bd69290a9f068fddfafed
Vincent Torri <vincent.torri@gmail.com>
2016-10-15 14:59:16 +0200

    View: fix compilation on Linux

--- src/bin/xpost_view.c
+++ src/bin/xpost_view.c
@@ -37,6 +37,15 @@
 #include <stdio.h>
 #include <string.h>
 
+#ifdef HAVE_XCB
+#include <xcb/xcb.h>
+#include <xcb/xcb_image.h>
+#include <xcb/xcb_aux.h>
+#include <xcb/xcb_icccm.h>
+#include <xcb/xcb_atom.h>
+#include <xcb/xcb_event.h>
+#endif
+
 #ifdef _WIN32
 #ifndef WIN32_LEAN_AND_MEAN
 # define WIN32_LEAN_AND_MEAN
@@ -48,23 +57,175 @@
 #include "xpost.h"
 #include "xpost_dsc.h"
 
-/* struct Xpost_View_Window */
-/* { */
-/* #ifdef HAVE_XCB */
-/*     xcb_connection_t *c; */
-/*     xcb_screen_t *scr; */
-/*     xcb_drawable_t win; */
-/*     int width, height; */
-/*     xcb_pixmap_t img; */
-/*     xcb_gcontext_t gc; */
-/*     xcb_colormap_t cmap; */
-/* #endif */
-/* }; */
+typedef struct _Xpost_View_Window Xpost_View_Window;
 
 #ifdef HAVE_XCB
-#elif defined _WIN32
 
-typedef struct _Xpost_View_Window Xpost_View_Window;
+struct _Xpost_View_Window
+{
+    xcb_connection_t *c;
+    xcb_screen_t *scr;
+    xcb_drawable_t window;
+    int width, height;
+    xcb_pixmap_t img;
+    xcb_gcontext_t gc;
+    xcb_colormap_t cmap;
+};
+
+static Xpost_View_Window *
+_xpost_view_win_new(int xorig, int yorig, int width, int height)
+{
+    xcb_screen_iterator_t iter;
+    xcb_get_geometry_reply_t *geom;
+    Xpost_View_Window *win;
+    int scrno;
+    unsigned int values[3];
+    unsigned int mask;
+    unsigned char depth;
+
+    win = (Xpost_View_Window *)malloc(sizeof(Xpost_View_Window));
+    if (!win)
+        return NULL;
+
+    win->c = xcb_connect(NULL, &scrno);
+    if (xcb_connection_has_error(win->c))
+    {
+        fprintf(stderr, "Fail to connect to the X server\n");
+        goto free_win;
+    }
+
+    iter = xcb_setup_roots_iterator(xcb_get_setup(win->c));
+    for (; iter.rem; --scrno, xcb_screen_next(&iter))
+    {
+        if (scrno == 0)
+        {
+            win->scr = iter.data;
+            break;
+        }
+    }
+
+    geom = xcb_get_geometry_reply(win->c,
+                                  xcb_get_geometry(win->c, win->scr->root), 0);
+    if (!geom)
+    {
+        fprintf(stderr, "Fail to the geometry of the root window\n");
+        goto disconnect_c;
+    }
+
+    depth = geom->depth;
+    free(geom);
+
+    win->window = xcb_generate_id(win->c);
+    mask = XCB_CW_BACK_PIXMAP | XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
+    values[0] = XCB_NONE;
+    values[1] = win->scr->white_pixel;
+    values[2] = XCB_EVENT_MASK_EXPOSURE;
+    xcb_create_window(win->c, XCB_COPY_FROM_PARENT,
+                      win->window, win->scr->root,
+                      0, 0,
+                      width, height,
+                      5,
+                      XCB_WINDOW_CLASS_INPUT_OUTPUT,
+                      win->scr->root_visual,
+                      mask,
+                      values);
+    xcb_icccm_set_wm_name(win->c, win->window,
+                          XCB_ATOM_STRING, 8, strlen("Xpost"), "Xpost");
+    xcb_map_window(win->c, win->window);
+    xcb_flush(win->c);
+
+    win->img = xcb_generate_id(win->c);
+    xcb_create_pixmap(win->c,
+                      depth, win->img,
+                      win->window, win->width, win->height);
+
+    win->gc = xcb_generate_id(win->c);
+    values[0] = win->scr->black_pixel;
+    values[1] = win->scr->white_pixel;
+    xcb_create_gc(win->c, win->gc, win->window,
+                  XCB_GC_FOREGROUND | XCB_GC_BACKGROUND,
+                  values);
+
+    win->cmap = xcb_generate_id(win->c);
+    xcb_create_colormap(win->c, XCB_COLORMAP_ALLOC_NONE, win->cmap,
+                        win->window, win->scr->root_visual);
+
+    win->width = width;
+    win->height = height;
+
+    return win;
+
+  disconnect_c:
+    xcb_disconnect(win->c);
+  free_win:
+    free(win);
+
+    return NULL;
+}
+
+static void
+_xpost_view_win_del(Xpost_View_Window *win)
+{
+    if (!win)
+        return;
+
+    xcb_disconnect(win->c);
+    free(win);
+}
+
+static void
+_xpost_view_main_loop(Xpost_View_Window *win)
+{
+    xcb_intern_atom_cookie_t cookie1;
+    xcb_intern_atom_cookie_t cookie2;
+    xcb_intern_atom_reply_t* reply1;
+    xcb_intern_atom_reply_t* reply2;
+    xcb_gcontext_t gc = { 0 };
+    int finished;
+
+    /*
+     * Listen to X client messages in order to be able to pickup
+     * the "delete window" message that is generated for example
+     * when someone clicks the top-right X button within the window
+     * manager decoration (or when user hits ALT-F4). */
+    cookie1 = xcb_intern_atom(win->c, 1,
+                              sizeof("WM_DELETE_WINDOW") - 1, "WM_DELETE_WINDOW");
+    cookie2 = xcb_intern_atom(win->c, 1,
+                              sizeof("WM_PROTOCOLS") - 1, "WM_PROTOCOLS");
+    reply1 = xcb_intern_atom_reply(win->c, cookie1, 0);
+    reply2 = xcb_intern_atom_reply(win->c, cookie2, 0);
+    xcb_icccm_set_wm_protocols (win->c, reply2->atom, win->window, 1, &reply1->atom);
+
+    finished = 0;
+    while (!finished)
+    {
+        xcb_generic_event_t *e;
+
+        if ((e = xcb_poll_for_event(win->c)))
+        {
+            switch (XCB_EVENT_RESPONSE_TYPE(e))
+            {
+                case XCB_EXPOSE:
+                    /* xcb_copy_area(win->c, win->pixmap, win->window, gc, */
+                    /*               0, 0, 0, 0, win->width, win->height); */
+                    xcb_flush(win->c);
+                    break;
+                case XCB_CLIENT_MESSAGE:
+                    if (((xcb_client_message_event_t *)e)->data.data32[0] == reply1->atom)
+                      finished = 1;
+                    break;
+                case XCB_BUTTON_PRESS:
+                    finished = 1;
+                    break;
+            }
+            free (e);
+        }
+
+        xcb_flush (win->c);
+    }
+}
+
+#elif defined _WIN32
 
 typedef struct
 {
@@ -135,7 +296,7 @@ _xpost_view_win_new(int xorig, int yorig, int width, int height)
 
     win->instance = GetModuleHandle(NULL);
     if (!win->instance)
-        return NULL;
+      goto free_win;
 
     icon = LoadImage(win->instance,
                      MAKEINTRESOURCE(101),
@@ -259,6 +420,9 @@ _xpost_view_win_new(int xorig, int yorig, int width, int height)
     UnregisterClass(TEXT("XPOST_VIEW"), win->instance);
   free_library:
     FreeLibrary(win->instance);
+  free_win:
+    free(win);
+
     return NULL;
 }
 
@@ -275,6 +439,29 @@ _xpost_view_win_del(Xpost_View_Window *win)
     UnregisterClass(TEXT("XPOST_VIEW"), win->instance);
     FreeLibrary(win->instance);
 }
+
+static void
+_xpost_view_main_loop(Xpost_View_Window *win)
+{
+    while(1)
+    {
+        MSG msg;
+        BOOL ret;
+
+        ret = PeekMessage(&msg, NULL, 0, 0, PM_REMOVE);
+        if (ret)
+        {
+            do
+            {
+                if (msg.message == WM_QUIT)
+                    return 0;
+                TranslateMessage(&msg);
+                DispatchMessageW(&msg);
+            } while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE));
+        }
+    }
+}
+
 #endif
 
 static void
@@ -383,7 +570,6 @@ _xpost_view_options_read(int argc, char *argv[], int *msg, const char **file)
 
 int main(int argc, char *argv[])
 {
-    MSG msg;
     Xpost_View_Window *win;
     Xpost_Context *ctx;
     const char *device;
@@ -424,23 +610,7 @@ int main(int argc, char *argv[])
     if (!win)
         return 0;
 
-    while(1)
-    {
-        MSG msg;
-        BOOL ret;
-
-        ret = PeekMessage(&msg, NULL, 0, 0, PM_REMOVE);
-        if (ret)
-        {
-            do
-            {
-                if (msg.message == WM_QUIT)
-                    return 0;
-                TranslateMessage(&msg);
-                DispatchMessageW(&msg);
-            } while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE));
-        }
-    }
+    _xpost_view_main_loop(win);
 
     /* xpost_quit(); */
 
